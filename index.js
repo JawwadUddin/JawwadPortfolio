ccd053226b8ba34aebce1c645299950fcd2f03e1
JawwadUddin <m.m.j.uddin12@gmail.com>
2021-08-12 21:46:22 +0100

    dynamic project render on projects page similar to a carousel

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-// generating background circles animation on the homepage 
+////////// generating background circles animation on the homepage 
 function createCircle() {
     const colors = ['#d4d1ff', '#9b99bd', '#6c6a7e', '#d1a2a2'];
     const bg = colors[Math.floor(Math.random()*colors.length)]
@@ -22,7 +22,7 @@ function createCircle() {
 
 setInterval(createCircle, 150)   
 
-//creating the typing effect on the homepage
+////////////creating the typing effect on the homepage
 
 const typingContent = ["I'm Jawwad Uddin", "This is my portfolio", "I enjoy creative experiences", "Let's collaborate"];
 let count = 0;
@@ -57,7 +57,7 @@ async function type() {
 
 const runType = setInterval(type, 50);
 
-//creating the navbar animations
+//////////creating the navbar animations
 
 const navSlide = () => {
     const burger = document.querySelector('.burger');
@@ -78,3 +78,77 @@ const navSlide = () => {
 }
 
 navSlide()
+
+/////////The carousel effect on the projects page
+
+//grabbing the elements in the DOM that need manipulation
+let projectImg = document.querySelector('.project-item img');
+let projectTitle = document.querySelector('.project-data h3');
+let projectDescription = document.querySelector('.project-data p');
+let projectURL = document.querySelector('.project-data a');
+let counter = 0;
+//Including all project data in an array
+let projectData = [
+    {
+        title: "Stride",
+        image_source: "images/stride.jpg",
+        desciption: "A full stack habit tracking app utilising HTML, CSS, Vanilla JS and express as front end technologies. A relational database namely PostregreSQL was used for the backend. Users were able to creare their own accounts and authentication was implemented using json web tokens.",
+        link: "https://the-stride.netlify.app/#login"
+    },
+    {
+        title: "Fact-Ory",
+        image_source: "images/fact-ory.jpg",
+        desciption: "My first ever full stack project. Fact-ory is place where users can post interesting facts. Users are able to add comments and react to facts other users may have submitted. HTML, JS and Tailwind CSS were used on the front-end, whilst the back-end was created with node.js and express. Additionally, an API was integrated to allow users to search and select a GIF they want to include in their post.",
+        link: "https://fact-ori.netlify.app/"
+    },
+    {
+        title: "Portfolio Website",
+        image_source: "images/mobile-version.jpg",
+        desciption: "In order to test the skills I had picked up over the past few weeks, I decided to challenge myself to create this portfolio without the aid of any libraries or frameworks. Everything on this page was created with HTML, CSS and Vanilla JS. It was a great experience overall, and it allowed me to visibly see the drastic devlopement that I had undergone in such short time.",
+        link: "#"
+    },
+    {
+        title: "Project 4",
+        image_source: "#",
+        desciption: "Coming soon ...",
+        link: "#"
+    },
+    {
+        title: "Project 5",
+        image_source: "#",
+        desciption: "Coming soon ...",
+        link: "#"
+    },
+]; 
+
+function displayProject(counter) {
+    projectTitle.textContent = projectData[counter].title;
+    projectImg.src = projectData[counter].image_source;
+    projectDescription.textContent = projectData[counter].desciption;
+    projectURL.href = projectData[counter].link;
+}
+
+displayProject(counter)
+
+//allowing arrows to move to another project
+let rightArrow = document.querySelector(".right-arrow");
+let leftArrow = document.querySelector(".left-arrow");
+
+rightArrow.addEventListener("click", () => {
+    if (counter === projectData.length -1 ) {
+        counter = 0;
+        displayProject(counter);
+    } else {
+        counter++;
+        displayProject(counter);
+    }
+})
+leftArrow.addEventListener("click", () => {
+    if (counter === 0) {
+        counter = projectData.length -1;
+        displayProject(counter);
+    } else {
+        counter--;
+        displayProject(counter);
+    }
+})
